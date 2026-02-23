34ad0c52c07cb9cbeef1d7a3a585810a22f8b912
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2026-02-23 23:32:43 +0530

    Imports

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "./Counter.sol";
+
 
 contract Counter {
     uint256 public number;
